004564ac99f6410f968417a2740749936d1b5a1d
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-02-28 01:49:07 +0300

    enemy following

--- Objects/Cannons/Cannon.gd
+++ Objects/Cannons/Cannon.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var enemies = []
+var cur_enemy
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta: float) -> void:
+	if enemies != []:
+		print("enemies not null")
+		cur_enemy = enemies[0]
+		look_at(cur_enemy.global_position)
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Vision_area_entered(area: Area2D) -> void:
+	print("body entered")
+	if(area.is_in_group("Enemies")):
+		enemies.append(area)
+		print("appended")
+
+
+func _on_Vision_area_exited(area: Area2D) -> void:
+	print("exited")
+	if(area.is_in_group("Enemies")):
+		enemies.erase(area)
--- Objects/Enemies/Enemy.gd
+++ Objects/Enemies/Enemy.gd
@@ -8,9 +8,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("READY")
 	pass # Replace with function body.
 
-
+func _physics_process(delta):
+	position.x += 2
+	#print("YEP")
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+
