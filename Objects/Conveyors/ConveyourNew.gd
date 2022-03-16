2b8167cdbc6cc459f8a71c88f34c84e3b883bb85
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-03-16 22:59:46 +0300

    Conv 1 bug left to go

--- Objects/Conveyors/ConveyourNew.gd
+++ Objects/Conveyors/ConveyourNew.gd
@@ -7,6 +7,8 @@ var isBuilding := false
 var isBuilt := false
 
 func StartBuilding() -> void:	# _startP: Vector2k
+	$Sprite.position = Vector2.ZERO
+	#$End.position = Vector2.ZERO
 	$End.rotation = $Sprite.rotation #- deg2rad(180)
 	$Sprite.rotation = $Sprite.position.angle_to_point($End.position) + deg2rad(90)
 	$Sprite.region_rect.size.y = to_local(get_global_mouse_position()).length()
@@ -24,9 +26,14 @@ func _process(delta: float) -> void:
 		return	# Not visible -> nothing to draw
 	if(isBuilding):		# просто пользовательская отрисовка
 		$End.rotation = $Sprite.rotation
-		$End.global_position = get_global_mouse_position()
+		$End.global_position = get_global_mouse_position()		# по-хорошему надо делать это в physics, но тогда есть баг с расстоянием между ласт тайлом конвеера и концом
+		#$End.global_position.y += 100
 		$Sprite.rotation = $Sprite.position.angle_to_point($End.position) + deg2rad(90)
-		$Sprite.region_rect.size.y = to_local(get_global_mouse_position()).length() + 1
+		#$Sprite.region_rect.size.y = to_local(get_global_mouse_position()).length() #+ 1
+		$Sprite.region_rect.size.y = $End.position.length()
+		#$Sprite.region_rect.size.x = $End.position.length()
+		#$Sprite.region_rect = Rect2(0,0,100,$End.global_position.length())
+		#$Sprite.set_region_rect = 
 		
 
 func _physics_process(delta: float) -> void:
