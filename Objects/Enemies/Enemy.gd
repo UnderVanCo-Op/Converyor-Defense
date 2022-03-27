52965658432f6a4b6fad2eeeaf9a8d5e9ea25f79
Terminator2006 <vania.pants@yandex.ru>
2022-03-27 17:52:37 +0300

    MainMenuPhoneVers1

--- Objects/Enemies/Enemy.gd
+++ Objects/Enemies/Enemy.gd
@@ -1,12 +1,13 @@
 extends PathFollow2D
 
-var speed = 100
+var speed = 5
 var hp = 10
 
 func _physics_process(delta: float) -> void:
 	# offset += speed		# smeshenie
-	var newOffset=get_offset()+speed*delta
+	var newOffset=get_offset()+speed
 	set_offset(newOffset)
+	
 	
 	
 
