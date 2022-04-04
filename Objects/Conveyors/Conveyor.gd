89af4958455224da193a0dde6e864f367ff25118
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-04-05 00:29:47 +0300

    Conveyor Milestone 3 in progress
    Trying to move cell from 1 conv to another

--- Objects/Conveyors/Conveyor.gd
+++ Objects/Conveyors/Conveyor.gd
@@ -4,10 +4,44 @@ extends Path2D
 var ConvCell := preload("res://Objects/Conveyors/ConvCell.tscn")
 var refToFirstCell = null		# stores reference to first cell in conv
 var StartPpos := Vector2.ZERO	# stores Vector2 position of a start Point
-var EndPpos := Vector2.ZERO		# store Vector2 position of a end Point
+var EndPpos := Vector2.ZERO		# stores Vector2 position of a end Point
 var isFull := false				# shows if the conveyor is fulled with cells
+var refToNextConv = null		# stores ref to the next conv in chains
+var isSending := false			# shows if conv is sending cells somewhere to next conv
+#var isContinue := false			# shows if the conveyor has a start in the end of the other conv
 
 signal StopCells()				# signal is emitted when cells are need to be stopped
+
+func ActivatePhysics() -> void:
+	set_physics_process(true)
+
+func DeactivatePhysics() -> void:
+	set_physics_process(false)
+
+
+# 
+func SetSendCellsTo(convPath : NodePath) -> void:
+	var conv = get_node_or_null(convPath)
+	if(conv):
+		refToNextConv = conv
+		isSending = true
+	else:
+		push_error("Conveyor_ERROR: can not get next conv in the chain")
+
+
+# 
+func ReceiveCell() -> void:
+	
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	if(isSending):
+		pass
+	elif(refToFirstCell and !isFull and refToFirstCell.unit_offset >= 1):
+		#print("First cell is in the end!")
+		isFull = true
+		emit_signal("StopCells")
 
 
 # Method adds one cell to conveyor
@@ -21,22 +55,15 @@ func AddCell() -> void:
 			#refToFirstCell.add_user_signal("FirstCellEnd")
 		#ref.connect("ReachedEnd", self, "s_CellReachedEnd")
 	else:
-		print("Conveyor_ERROR: Can't add cell bcs Conv is full")
-
-
-func _physics_process(delta: float) -> void:
-	if(refToFirstCell and !isFull and refToFirstCell.unit_offset >= 1):
-		#print("First cell is in the end!")
-		isFull = true
-		emit_signal("StopCells")
+		push_error("Conveyor_ERROR: Can't add cell bcs Conv is full")
 
 
 # Method fulls Conveyor with cells while first cell is not in the end
 func FullWithCells() -> void:		# mb add some bool to ensure spawning or getting an error
 	if(curve.get_point_count() == 0):
-		print("Conveyor_ERROR: 0 points in curve for conveyor!")
+		push_error("Conveyor_ERROR: 0 points in curve for conveyor!")
 		return
 	#print(curve.get_baked_length())
 	while(!isFull):
-		AddCell()											# 7 cells if addcell is pre yield, otherwise 8
-		yield(get_tree().create_timer(0.3333333), "timeout")	# 0.3333 needs for fixing disctance btw
+		AddCell()												# 7 cells if addcell is pre yield, otherwise 8
+		yield(get_tree().create_timer(0.3333333333), "timeout")	# 0.3333... needs for fixing disctance btw
