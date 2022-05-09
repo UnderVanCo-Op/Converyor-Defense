cd4026647dd6d97e98e6d9067582b63a5a96beef
Terminator2006 <vania.pants@yandex.ru>
2022-05-09 13:30:42 +0300

    add AnimationSprite for enemy not ready yet

--- Objects/Enemies/Enemy.gd
+++ Objects/Enemies/Enemy.gd
@@ -2,26 +2,35 @@ extends PathFollow2D
 # This is Enemy.gd
 
 var speed = 4
-var hp = 10
+var hp = 40
 
-
+func _ready():
+	$AnimatedSprite.play("default")
+	
 func _physics_process(delta: float) -> void:
 	offset += speed
 	#if unit_offset >= 1:
-		#queue_free()
+		#queue_free()	
 	if (unit_offset>=0.99):
 		print("ENDMAP")
 		get_parent().get_parent().get_parent().get_node("GUI/ProgressBar").ProgresBarHP()
-		
 		queue_free()
 
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Projectile"):
+		$AnimatedSprite.play("hit")
 		area.queue_free()
 		hp -=5
 		if hp <= 0:
 			#print("enemy died")
 			$"../../../GameManager".call("change_money", 15)
 			queue_free()
+		
+
+
+
+
+
+
 
